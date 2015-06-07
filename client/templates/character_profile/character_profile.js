deedd8d491d233123807793a786415ffcb2908d5
Julio Mansilla <Mansillajb@Julios-MacBook-Air.local>
2015-06-07 14:33:07 -0400

    Create Abilities objectfor DB.

--- client/templates/character_profile/character_profile.js
+++ client/templates/character_profile/character_profile.js
@@ -1,11 +1,71 @@
 Template.characterProfile.events({
 	"click #saveCharacter":function (event, template){
-		var str = template.find("#str").value
-		var strength = {};
-		strength.score = str;
+
+		// Ability Scores
+		var strength = template.find("#str-score").value;
+		var dexterity = template.find("#dex-score").value;
+		var constitution = template.find("#con-score").value;
+		var intellect = template.find("#int-score").value;
+		var wisdom = template.find("#wis-score").value;
+		var charisma = template.find("#cha-score").value;
+
+		//Item Bonus
+		var strengthBonus = template.find("#str-bonus").value;
+		var dexterityBonus = template.find("#dex-bonus").value;
+		var constitutionBonus = template.find("#con-bonus").value;
+		var intellectBonus = template.find("#int-bonus").value;
+		var wisdomBonus = template.find("#wis-bonus").value;
+		var charismaBonus = template.find("#cha-bonus").value;
+
+		//Temp Score
+		var strengthTemp = template.find("#str-temp").value;
+		var dexterityTemp = template.find("#dex-temp").value;
+		var constitutionTemp = template.find("#con-temp").value;
+		var intellectTemp = template.find("#int-temp").value;
+		var wisdomTemp = template.find("#wis-temp").value;
+		var charismaTemp = template.find("#cha-temp").value;
+
+		//BSON object for the database
+		var abilities = {
+			STR:{
+				score:strength,
+				bonus:strengthBonus,
+				temp:strengthTemp
+			},
+			DEX:{
+				score:dexterity,
+				bonus:dexterityBonus,
+				temp:dexterityTemp
+			}
+			CON:{
+				score:constitution,
+				bonus:constitutionBonus,
+				temp:constitutionTemp
+			},
+			INT:{
+				score:intellect,
+				bonus:intellectBonus,
+				temp:intellectTemp
+			},
+			WIS:{
+				score:wisdom,
+				bonus:wisdomBonus,
+				temp:wisdomTemp
+			},
+			CHA:{
+				score:charisma,
+				bonus:charismaBonus,
+				temp:charismaTemp
+			}
+		};
+
 		
-		console.log(str);
-		Characters.update(this._id, {$set:{str:strength}})
-		template.find("#str").value="";
+		console.log( "Pushing it real good " + Meteor.user().username );
+		Characters.update( this._id, { $set:{ abilities:abilities } } );
+		template.find( "#str" ).value="";
 	}
 });
+
+
+
+
